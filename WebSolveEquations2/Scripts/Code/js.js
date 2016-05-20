0f49de3d39e637c1b0478ad9e94eb6289bd863d6
Veronika <mainyvar@gmail.com>
2016-05-20 15:25:41 +0500

    fixed

--- WebSolveEquations2/Scripts/Code/js.js
+++ WebSolveEquations2/Scripts/Code/js.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function(){
     var gear = document.getElementById("gear");
     var start_elem = $('.site-welcome');
+    var count = 0;
     $('.site-welcome').on('mousewheel', function (event) {
         var delta = event.originalEvent.wheelDeltaY;
         if(delta < 0)
@@ -8,6 +9,19 @@
         if (delta > 0) {
             $(this).css('top', '0');
         }
+    });
+    $('#gear').on('mousewheel', function (event) {
+        //var delta = event.originalEvent.wheelDeltaY;
+        var delta = event.originalEvent.movementY;
+        if (delta < 0) {
+            count += delta;
+            gear.style.transform = "rotate(" + delta * 0.1 + "deg)";
+        }
+        if (delta > 0) {
+            count -= delta;
+            gear.style.transform = "rotate(" - delta * 0.1 + "deg)";
+        }
+        console.log(delta);
     });
     $('.more').on('click', function () {
         $('.site-welcome').css('top', '-80%');
